9ec393f425fe142580a5cfed23d36ce8a629474c
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-04-08 17:20:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,13 +9,7 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
- });
-client.on('message'. message => {
-    if (message.content === 'help') {
-        message.reply('You can use this soon!');
-                
-    }
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
+client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot

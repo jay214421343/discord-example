3face7cada6d364cca33687bf0a3b5032440e8ba
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-04-08 17:11:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('message', message => {
  });
 client.on('message'. message => {
     if (message.content === 'help') {
-        message.reply('You can use this soon!);
+        message.reply('You can use this soon!');
                 
     }
 });
